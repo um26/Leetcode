d2af625e5115d4536b2fd68cf5d65bd138783804
utkarsh <123744256+um26@users.noreply.github.com>
2025-06-27 23:47:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,23 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    return new int[]{i, j};
-                }
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        int i=0;
+        int j=nums.length -1;
+        while(i<nums.length && j>0){
+            if(nums[i]+nums[j]==target){
+                return new int[]{i, j};
             }
+            else{
+                i++; j--;
+            }
+            
         }
     return null;}
-} 
-
-
-
+}
