220ba4c142f5d27fbe595c1aa35c6088a7ecaf26
utkarsh <123744256+um26@users.noreply.github.com>
2025-07-06 02:03:48 +0530

    Time: 0 ms (100%), Space: 48.9 MB (73.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -3,8 +3,8 @@ class Solution {
         int n=s.length;
         for(int i=0;i<n/2;i++){
             char temp=s[i];
-            s[i]=s[n-i-1];
-            s[n-i-1]=temp;
+            s[i]=s[n-1-i];
+            s[n-1-i]=temp;
         }
     }
 }
