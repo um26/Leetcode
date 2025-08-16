96dd53987367a9c723ab3e87012b7f03a45f87e2
utkarsh <123744256+um26@users.noreply.github.com>
2025-08-16 15:05:09 +0530

    Time: 1 ms (35.6%), Space: 40.6 MB (56.69%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximum69Number (int num) {
+        StringBuilder s = new StringBuilder(Integer.toString(num));
+        for(int i =0; i< s.length();i++){
+            if(s.charAt(i)=='6'){
+                s.setCharAt(i,'9');
+                break;
+            }
+        }
+        int n =Integer.parseInt(s.toString());
+        return n;
+    }
+}
