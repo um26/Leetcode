c2b7e6ec081923d7d21e87cd96f9ec5ef978b3c6
utkarsh <123744256+um26@users.noreply.github.com>
2025-08-30 00:20:23 +0530

    Time: 0 ms (100%), Space: 40.6 MB (86.88%) - LeetHub

---
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        long oddN = (n + 1) / 2;
+        long evenN = n / 2;
+        long oddM = (m + 1) / 2;
+        long evenM = m / 2;
+        
+        return oddN * evenM + evenN * oddM;
+    }
+}
