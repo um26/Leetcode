51afbcbee5ffdaf6bbc17a5b8a150f0a631a77e4
utkarsh <123744256+um26@users.noreply.github.com>
2025-09-07 17:16:38 +0530

    Time: 5 ms (6.54%), Space: 41.7 MB (85.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,43 +1,16 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-    //     Stack<Character> stack= new Stack<Character> ;
-    //     int length=s.length;
-    //     for(int i=0;i<length;i++){
-    //         char ch=s.charat(i);
-    //         if(ch=='(' || ch=='{' || ch=='['){
-    //             stack.push(ch);
-    //         }
-    //         else{
-    //             if(stack.isEmpty()) return false;
-
-    //             char top= stack.peek();
-    //             if(ch==')' && top!='(' || ch=='}' && top!='{' || ch==']' && ch!='[') return false;
-    //             stack pop;
-    //         }
-
-    //     }
-
-    //     return stack.isEmpty();
-    // }
-
-        Stack<Character> stack= new Stack<Character>();
-        int l=s.length();
-        for(int i=0;i<l;i++){
-            char ch= s.charAt(i); //**imp.**
-            if(ch=='(' || ch=='{' || ch=='['){
-            stack.push(ch);
+        Stack<Character> stack= new Stack<>(); 
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
             }
-            else{ 
-                if (stack.isEmpty()) return false;
-                if(ch==')' && stack.peek()!='(' || ch=='}' && stack.peek()!='{' || ch==']' && stack.peek()!='[')
-                { return false;
-                }
-                stack.pop();
-            }
+            if(stack.isEmpty()) return false;
+            else if(s.charAt(i)==')' && stack.pop()!='(') return false;
+            else if(s.charAt(i)=='}' && stack.pop()!='{') return false;
+            else if(s.charAt(i)==']' && stack.pop()!='[') return false;
         }
-
-    return stack.isEmpty();
+        if(stack.size()!=0) return false;
+        return true;
     }
 }
