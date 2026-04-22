e20638f6e65dd43c101604c0fec60292ab1c423d
utkarsh <123744256+um26@users.noreply.github.com>
2026-04-22 10:14:17 +0200

    Time: 7 ms (96.57%), Space: 19.5 MB (50.49%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        def is_valid(q, d):
+            diff = 0
+            for i in range(len(q)):
+                if q[i] != d[i]:
+                    diff += 1
+                    if diff > 2:
+                        return False
+            return True
+        ans = []
+        for q in queries:
+            for d in dictionary:
+                if is_valid(q, d):
+                    ans.append(q)
+                    break
+        return ans
