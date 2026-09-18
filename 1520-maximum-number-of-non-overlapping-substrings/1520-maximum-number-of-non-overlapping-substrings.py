c6d5bbb808a116cf2af69838395128cfbda0a5c1
utkarsh <123744256+um26@users.noreply.github.com>
2026-09-18 08:23:48 +0530

    Time: 70 ms (96.46%), Space: 20.3 MB (70.8%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
@@ -1,28 +1,26 @@
 class Solution:
     def maxNumOfSubstrings(self, s: str) -> List[str]:
-        m={}
-        for i,ch in enumerate(s):
-            if ch not in m: m[ch]=[i,i]
-            else: m[ch][1] = i
-        res, st, start = [], [], -1
-        for ind,ch in enumerate(s):
-            if st and st[-1][1] == ind:
-                i,j,_ = st.pop()
-                if i>start:
-                    res.append(s[i:j+1])
-                start=j
-                continue
-            i,j = m[ch]
-            setch = {ch}
-            if i==j:
-                res.append(s[i])
-                start=j
-                continue
-            while st and (st[-1][0]>=i or st[-1][1]<=j or ch in st[-1][2]):
-                x = st.pop()
-                i,j = min(i, x[0]), max(j, x[1])
-                setch.update(x[2])
-            st.append((i,j,setch))
-        return res
-            
+        n = len(s)
+        counts = Counter(s)
+        first = {k: s.find(k) for k in counts}
+        last = {k: s.rfind(k) for k in counts}
         
+        res = []
+        queue = deque()
+
+        for k in counts:
+            queue.appendleft([first[k], last[k], counts[k]])
+            left, right, total = inf, -inf, 0
+
+            for x, y, z in queue:
+                total += z
+                left = min(left, x)
+                right = max(right, y)
+                if total == right - left + 1:
+                    break
+
+            if total == right - left + 1:
+                res.append(s[left:right+1])
+                queue = deque()
+
+        return res
